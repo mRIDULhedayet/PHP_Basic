d1eed14fe1b46b702d1cf8161ac01b6ab30bdfcc
mRIDULhedayet <119140283+mRIDULhedayet@users.noreply.github.com>
2023-06-22 01:58:21 +0600

    Update basic01.php

--- basic01.php
+++ basic01.php
@@ -4,5 +4,15 @@ echo "Welcome to PHP."."<br>";
 
  ?>
 
- 
-<?="This is my First php code" ?>
+<?="This is my First php code"."<br>"?>
+
+<?php
+echo "What's on your mind ?"."<br>";
+
+echo ("\n What's on your mind ?"."<br>");
+
+echo "This"," is"," my"," First "," php "," code <br>";
+
+echo ("This"),(" is"),(" my"),(" First "),(" php "),(" code");
+
+?>
