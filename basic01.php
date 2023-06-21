5d777b42af29a096ae3a84688bfcb475676b57ac
mRIDULhedayet <119140283+mRIDULhedayet@users.noreply.github.com>
2023-06-22 02:17:47 +0600

    Update basic01.php

--- basic01.php
+++ basic01.php
@@ -4,15 +4,28 @@ echo "Welcome to PHP."."<br>"; # echo in php
 
  ?>
 
-<?="This is my First php code"."<br>" # short form to echo in php ?>  
+<?="This is my First php code."."<br>" # short form to echo in php ?>  
 
 <?php
 echo "What's on your mind ?"."<br>"; # echo without first bracket
 
 echo (" What's on your mind ?"."<br>"); #echo with first bracket
 
-echo "This"," is"," my"," First "," php "," code <br>"; # echo multiple string without first bracket
+echo "This"," is"," my"," First "," php "," code. <br>"; # echo multiple string without first bracket
 
-echo ("This"),(" is"),(" my"),(" First "),(" php "),(" code"); # echo multiple string with first bracket
+echo ("This"),(" is"),(" my"),(" First "),(" php "),(" code."); # echo multiple string with first bracket
+
+
+
+
+//Output: 
+// Welcome to PHP.
+// This is my First php code.
+// What's on your mind ?
+// What's on your mind ?
+// This is my First php code.
+// This is my First php code.
+
+
 
 ?>
