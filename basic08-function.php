0ae58dacec3152073d2a9a39f239df11c13a0d35
mRIDULhedayet <119140283+mRIDULhedayet@users.noreply.github.com>
2023-06-22 06:04:33 +0600

    Create basic08-function.php

---
+++ basic08-function.php
@@ -0,0 +1,27 @@
+<?php
+$name=" Mridul";
+function functionName($name){
+ echo "My Name is".$name." .";
+}
+functionName($name);   //Output: My Name is Mridul .
+echo "<br>";
+
+
+function fName(){
+ echo "Mridul";
+}
+echo "I am ";  //Output: I am Mridul
+fName();
+
+
+
+echo "<br>";
+echo " My name is ";  //Output: My name is Mridul
+myName();
+function myName(){
+ echo "Mridul";
+
+}
+
+
+ ?>
